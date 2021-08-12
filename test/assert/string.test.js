e82c79a33135582ba4e510ebf00b5b60467c585b
Damien Maillard <damien.maillard@dailymotion.com>
2021-08-12 19:50:40 +0200

    add test for string using DEL control char to ensure it's not displayed as ""

---
+++ test/assert/string.test.js
@@ -0,0 +1,21 @@
+import { assert } from "@jsenv/assert"
+import { ensureAssertionErrorWithMessage } from "@jsenv/assert/test/ensureAssertionErrorWithMessage.js"
+
+{
+  const actual = String.fromCharCode(127)
+  const expected = ""
+  try {
+    assert({ actual, expected })
+  } catch (e) {
+    ensureAssertionErrorWithMessage(
+      e,
+      `unequal values.
+--- found ---
+"\\x7F"
+--- expected ---
+""
+--- at ---
+value`,
+    )
+  }
+}
