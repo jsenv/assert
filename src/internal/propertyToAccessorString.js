c1bdb5824382d65d01cdec12541dacd449fa561d
Damien Maillard <damien.maillard@dailymotion.com>
2021-10-15 16:16:42 +0200

    remove circular dependency

--- src/internal/propertyToAccessorString.js
+++ src/internal/propertyToAccessorString.js
@@ -1,5 +1,5 @@
 import { inspect } from "@jsenv/inspect"
-import { symbolToWellKnownSymbol } from "./symbolToWellKnownSymbol.js"
+
 import { propertyNameToDotNotationAllowed } from "./propertyNameToDotNotationAllowed.js"
 
 export const propertyToAccessorString = (property) => {
@@ -15,4 +15,32 @@ export const propertyToAccessorString = (property) => {
   }
 
   return `[${symbolToWellKnownSymbol(property)}]`
+}
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Symbol#Well-known_symbols
+const symbolToWellKnownSymbol = (symbol) => {
+  const wellKnownSymbolName = Object.getOwnPropertyNames(Symbol).find(
+    (name) => symbol === Symbol[name],
+  )
+  if (wellKnownSymbolName) {
+    return `Symbol${propertyToAccessorString(wellKnownSymbolName)}`
+  }
+
+  const description = symbolToDescription(symbol)
+  if (description) {
+    const key = Symbol.keyFor(symbol)
+    if (key) {
+      return `Symbol.for(${inspect(description)})`
+    }
+    return `Symbol(${inspect(description)})`
+  }
+  return `Symbol()`
+}
+
+const symbolToDescription = (symbol) => {
+  const toStringResult = symbol.toString()
+  const openingParenthesisIndex = toStringResult.indexOf("(")
+  const closingParenthesisIndex = toStringResult.indexOf(")")
+  return toStringResult.slice(openingParenthesisIndex + 1, closingParenthesisIndex)
+  // return symbol.description // does not work on node
 }
